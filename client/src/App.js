a2d67855b5c69a0913ef227aa3c10229da547599
yukirochi <justineabanilla81@gmail.com>
2025-09-17 23:43:20 +0800

    the shop ui needed to be change because the category doesnt fit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import LandinPage from "./pages/Landing/LandingPage";
 import { Routes,Route } from "react-router-dom";
 import LoginPage from "./pages/Auth/LoginPage";
 import RegisterPage from "./pages/Auth/SignupPage"
-import TestPage from "./pages/Auth/textpage";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Route path='/' element={<LandinPage></LandinPage>}/>
       <Route path='/auth/login' element={<LoginPage></LoginPage>} ></Route>
       <Route path="/auth/register" element={<RegisterPage></RegisterPage>}></Route>
-      <Route path="/test" element={<TestPage></TestPage>}></Route>
+      <Route path="/shop" element={<ShopPage></ShopPage>}></Route>
     </Routes>
   );
 }
---
+++ client/src/pages/shop/shop.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navigator from "./nav"
+import axios from "axios";
+function ShopPage() {
+   
+    return ( 
+       <Navigator></Navigator>
+        
+    );
+}
+
+export default ShopPage;
