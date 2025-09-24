f5a1470743de21c9a85323a239cfda298befafe6
yukirochi <justineabanilla81@gmail.com>
2025-09-24 23:55:30 +0800

    product page 24/09/25

--- client/src/pages/shop/shop.jsx
+++ client/src/pages/shop/shop.jsx
@@ -4,12 +4,14 @@ import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import ShopContent from "./shopContent";
 import Shopfooter from "./shopfooter";
+import ProductPage from "./productPage";
 function ShopPage() {
   return (
     <div className=" overflow-x-hidden">
       <Navigator></Navigator>
       <Routes>
         <Route path="categories/:id" element={<ShopContent></ShopContent>}></Route>
+        <Route path="product/:id/:title" element={<ProductPage></ProductPage>}></Route>
       </Routes>
       <Shopfooter></Shopfooter>
     </div>
