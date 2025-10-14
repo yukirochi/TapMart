8d698fd99b9b45e0eec73a7c366eb11f319b1a1a
yukirochi <justineabanilla81@gmail.com>
2025-10-14 23:51:05 +0800

    10/14/25

--- client/src/pages/home/homeContent.jsx
+++ client/src/pages/home/homeContent.jsx
@@ -53,7 +53,7 @@ function HomeContent() {
         <div class="flex-col gap-4 w-full flex items-center justify-center">
           <div class="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
             <div class="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
-         contents here </div>
+         contents heree </div>
         </div>
       </div>
     );
