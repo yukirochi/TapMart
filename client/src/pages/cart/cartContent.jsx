e6d3ada903768f4b9bd18be08e2d4b048a97f22b
yukirochi <justineabanilla81@gmail.com>
2025-11-04 20:06:17 +0800

    simple cart modification

--- client/src/pages/cart/cartContent.jsx
+++ client/src/pages/cart/cartContent.jsx
@@ -293,9 +293,61 @@ export default () => (
                                     {item.job_description}
                                 </p>
                                 <div className="text-sm text-gray-600 flex items-center gap-6">
-                                    <span className="flex items-center gap-2">
-                                     <span>⬅️</span><span>1</span><span>➡️</span>
-                                    </span>
+                                    <div class="relative flex items-center max-w-[8rem] mt-2">
+                    <button
+                      type="button"
+                      id="decrement-button"
+            
+                      class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                    >
+                      <svg
+                        class="w-3 h-3 text-gray-900 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 18 2"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M1 1h16"
+                        ></path>
+                      </svg>
+                    </button>
+                    <input
+                      type="text"
+                      id="quantity-input"
+                      data-input-counter=""
+                      aria-describedby="helper-text-explanation"
+                      class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="999"
+                      required=""
+             
+                    />
+                    <button
+                      type="button"
+                      id="increment-button"
+                      class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                    >
+                      <svg
+                        class="w-3 h-3 text-gray-900 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 18 18"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M9 1v16M1 9h16"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
                                     <button className="flex items-center gap-2">
                                       
 
